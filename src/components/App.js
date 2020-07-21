0105807f7045c83b754c5ff1ce86dfc8846cf213
Karthick Rajasekaran <kraj0008@student.monash.edu>
2020-07-21 20:51:10 +1000

    "Hooks added"

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
+
+function App() {
+  function getPage() {
+    const route = window.location.pathname;
+    console.log("The route is", route);
+    if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
+    return <HomePage />;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div className="jumbotron">
+      <h1>Hello Karthick</h1>
+      <p>Welcome to React</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default Homepage;
